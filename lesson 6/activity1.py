e80cf85e6982ec8d1c8efd91d0c03267e7fa2219
sanavsinha <sanavsinha@gmail.com>
2026-05-13 20:55:25 +0530

    activity 1

---
+++ lesson 6/activity1.py
@@ -0,0 +1,20 @@
+a = 65
+b = 69
+c = 90
+if a and b and c :
+    print("all the values are true")
+else :
+    print("at least one is false")
+
+a= 41
+b=-10
+c=5
+if a>0 or b>0 :
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
+
+if b>0 or c>0 :
+    print("either of the number is greater than 0")
+else:
+    print("no number is greater than 0")
