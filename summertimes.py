db2fdbd21eb9636a8167b8f3ab2f581e33bd2a43
sanavsinha <sanavsinha@gmail.com>
2026-06-10 20:02:36 +0530

    clothe advisor

---
+++ summertimes.py
@@ -0,0 +1,6 @@
+temp_str = input("what is the temprature outside(in celsius)")
+temprature = float(temp_str)
+if temprature > 30 :
+    print("its warm time for light clothes ")
+else:
+    print("you can wear a jacket and pull over")
